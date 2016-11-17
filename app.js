6a1bbcb250b124c71e9e8dcaf2db54e4ba102599
hotta <g1327061@st.t-kougei.ac.jp>
2016-11-17 14:56:15 +0900

    外部ファイル化するためのコミット?

---
+++ app.js
@@ -0,0 +1,17 @@
+// 1.モジュールオブジェクトの初期化
+var fs = require("fs");
+var server = require("http").createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var output = fs.readFileSync("./index.html", "utf-8");
+    res.end(output);
+}).listen(8080);
+var io = require("socket.io").listen(server);
+
+// ユーザ管理ハッシュ
+var userHash = {};
+
+// 2.イベントの定義
+io.sockets.on("connection", function (socket)
+{
+    log('connected');
+});
---
+++ main.js
@@ -0,0 +1,61 @@
+var socket = io.connect('http://localhost:8080');
+
+enchant();
+
+window.onload = function ()
+{
+    var core = new Core(640, 360);
+    core.preload('chara1.png');
+    core.fps = 15;
+    core.onload = function () {
+        var bear = new Sprite(32, 32);
+        bear.image = core.assets['chara1.png'];
+        bear.x = 0;
+        bear.y = 0;
+        bear.frame = 1;
+
+        //キーボード操作時
+        bear.addEventListener('enterframe', function ()
+        {
+            if (core.input.left)
+            {
+                this.x -= 5;
+                this.scale = (-1, 1);
+                this.frame = this.age % 3;
+            }
+            if (core.input.right)
+            {
+                this.x += 5;
+                this.frame = this.age % 3;
+            }
+            if (core.input.up)
+            {
+                this.y -= 5;
+                this.frame = this.age % 3;
+            }
+            if (core.input.down)
+            {
+                this.y += 5;
+                this.frame = this.age % 3;
+            }
+        });
+
+        //スマホでも操作できるようにタッチ反応型
+        bear.on('touchstart', function ()
+        {
+            //クマを消す
+            //core.rootScene.removeChild(this);
+        });
+
+        //クマを座標に動かしたい時は本体からの座標を受け取って移動するようにする。
+        core.rootScene.on('touchstart', function(pos)
+        {
+            bear.x = pos.x;
+            bear.y = pos.y;
+        });
+
+        //オブジェクトに追加する処理(ここに入れたいオブジェクトを指定しないとなにも起きない)
+        core.rootScene.addChild(bear);
+    }
+    core.start();
+};
