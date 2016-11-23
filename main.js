4499902a7a17870b62779942879ec0884ba2dab8
hotta <g1327061@st.t-kougei.ac.jp>
2016-11-23 22:30:09 +0900

    サーバーがつなげられるかの確認

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-var socket = io.connect('http://localhost:8888');
+var socket = io.connect('https://safe-reef-35714.herokuapp.com/');
 
 enchant();
 
 window.onload = function ()
 {
     var core = new Core(640, 360);
+    //事前にロードを行う
+    core.preload('back5.png');
     core.preload('chara1.png');
+    //fpsの設定
     core.fps = 15;
-    core.onload = function () {
+
+    core.onload = function ()
+    {
+        //スプライト情報の入力
+        //キャラクター
         var bear = new Sprite(32, 32);
         bear.image = core.assets['chara1.png'];
         bear.x = 0;
         bear.y = 0;
         bear.frame = 1;
+
+        //背景
+        var back = new Sprite(640, 360);
+        back.image = core.assets['back5.png'];
+        back.x = 0;
+        back.y = 0;
+        back.frame = 1;
 
         //キーボード操作時
         bear.addEventListener('enterframe', function ()
@@ -54,8 +68,9 @@ window.onload = function ()
             bear.y = pos.y;
         });
 
-        //オブジェクトに追加する処理(ここに入れたいオブジェクトを指定しないとなにも起きない)
-        core.rootScene.addChild(bear);
+        //オブジェクトに追加する処理(ここに入れたいオブジェクトを描画順に指定)
+        core.rootScene.addChild(back);  //一番背景
+        core.rootScene.addChild(bear);  //一番前面
     }
     core.start();
 };
