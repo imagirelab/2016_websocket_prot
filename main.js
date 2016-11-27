d36b3d5b0c20c0d214789839f5f36c2c074aa12f
hotta <g1327061@st.t-kougei.ac.jp>
2016-11-27 20:19:34 +0900

    UI素材の追加ソケット送信確認

--- main.js
+++ main.js
@@ -1,76 +1,115 @@
 var socket = io.connect('https://safe-reef-35714.herokuapp.com/');
 
+var id = "";
+
+socket.on("connect", function ()
+{
+    id = socket.socket.sessionid;
+});
+
 enchant();
 
 window.onload = function ()
 {
     var core = new Core(640, 360);
+
+    var Type = {
+        PUPU: 'PUPU',
+        POPO: 'POPO',
+        PIPI: 'PIPI',
+    };
+
+    var Direction = {
+        Top: 'TOP',
+        Middle: 'MIDDLE',
+        Buttom: 'BUTTOM',
+    };
+
     //事前にロードを行う
-    core.preload('back5.png');
-    core.preload('chara1.png');
+    core.preload('img/back5.png');
+    core.preload('img/pupu.png');
+    core.preload('img/pipi.png');
+    core.preload('img/popo.png');
     //fpsの設定
     core.fps = 15;
 
     core.onload = function ()
     {
         //スプライト情報の入力
-        //キャラクター
-        var bear = new Sprite(32, 32);
-        bear.image = core.assets['chara1.png'];
-        bear.x = 0;
-        bear.y = 0;
-        bear.frame = 1;
+        //ボタン
+        var pupuBtn = new Sprite(1200, 1200);
+        pupuBtn.image = core.assets['img/pupu.png'];
+        pupuBtn.scaleX = 0.05;
+        pupuBtn.scaleY = 0.05;
+        pupuBtn.x = -50;
+        pupuBtn.y = -500;
+        pupuBtn.frame = 1;
+
+        var popoBtn = new Sprite(1200, 1200);
+        popoBtn.image = core.assets['img/popo.png'];
+        popoBtn.scaleX = 0.05;
+        popoBtn.scaleY = 0.05;
+        popoBtn.x = -50;
+        popoBtn.y = -400;
+        popoBtn.frame = 1;
+
+        var pipiBtn = new Sprite(1200, 1200);
+        pipiBtn.image = core.assets['img/pipi.png'];
+        pipiBtn.scaleX = 0.05;
+        pipiBtn.scaleY = 0.05;
+        pipiBtn.x = -50;
+        pipiBtn.y = -300;
+        pipiBtn.frame = 1;
 
         //背景
         var back = new Sprite(640, 360);
-        back.image = core.assets['back5.png'];
+        back.image = core.assets['img/back5.png'];
         back.x = 0;
         back.y = 0;
         back.frame = 1;
 
-        //キーボード操作時
-        bear.addEventListener('enterframe', function ()
+        ////スマホでも操作できるようにタッチ反応型
+        //bear.on('touchstart', function ()
+        //{
+        //    //クマを消す
+        //    //core.rootScene.removeChild(this);
+        //});
+
+        //ボタンが押された時の処理
+        pupuBtn.on('touchstart', function ()
         {
-            if (core.input.left)
-            {
-                this.x -= 5;
-                this.scale = (-1, 1);
-                this.frame = this.age % 3;
-            }
-            if (core.input.right)
-            {
-                this.x += 5;
-                this.frame = this.age % 3;
-            }
-            if (core.input.up)
-            {
-                this.y -= 5;
-                this.frame = this.age % 3;
-            }
-            if (core.input.down)
-            {
-                this.y += 5;
-                this.frame = this.age % 3;
-            }
+            Type = Type.PUPU;
+            Direction = Direction.Middle;
+            socket.emit("DemonPush", { Type: Type, Direction: Direction, Level: 1, PlayerID: id })
+            log("Pushed");
         });
 
-        //スマホでも操作できるようにタッチ反応型
-        bear.on('touchstart', function ()
+        popoBtn.on('touchstart', function ()
         {
-            //クマを消す
-            //core.rootScene.removeChild(this);
+            Type = Type.POPO;
+            Direction = Direction.Middle;
+            socket.emit("DemonPush", { Type: Type, Direction: Direction, Level: 1, PlayerID: id })
+            log("Pushed");
         });
 
-        //クマを座標に動かしたい時は本体からの座標を受け取って移動するようにする。
-        core.rootScene.on('touchstart', function(pos)
+        pipiBtn.on('touchstart', function ()
         {
-            bear.x = pos.x;
-            bear.y = pos.y;
+            Type = Type.PIPI;
+            Direction = Direction.Middle;
+            socket.emit("DemonPush", { Type: Type, Direction: Direction, Level: 1, PlayerID: id })
+            log("Pushed");
         });
+
+        ////クマを座標に動かしたい時は本体からの座標を受け取って移動するようにする。
+        //core.rootScene.on('touchstart', function(pos)
+        //{
+        //});
 
         //オブジェクトに追加する処理(ここに入れたいオブジェクトを描画順に指定)
         core.rootScene.addChild(back);  //一番背景
-        core.rootScene.addChild(bear);  //一番前面
+        core.rootScene.addChild(pupuBtn);  //一番前面
+        core.rootScene.addChild(popoBtn);  //一番前面
+        core.rootScene.addChild(pipiBtn);  //一番前面
     }
     core.start();
 };
