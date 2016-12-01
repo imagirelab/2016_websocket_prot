6d5e868360cffa7b3d2d8fb70cae7853a4d60fed
hotta <g1327061@st.t-kougei.ac.jp>
2016-12-02 03:56:33 +0900

    悪魔召喚の表示をわかりやすく変更・CPが１秒毎に一定数コスト増加できるように変更(main.jsの変数を変更することで変更可能)

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ enchant();
 
 window.onload = function ()
 {
-    var core = new Core(640, 360);
+    var core = new Core(3200, 1800);
 
     //悪魔                Type    Dir  Level ID   COST HP   ATK  SPEED    
     var PUPU = new Demon("PUPU", "None", 0, null, 100, 200, 500, 6);
@@ -24,7 +24,7 @@ window.onload = function ()
     var MaxCost = 3000;
 
     //毎秒取得できるコスト
-    var fpsCost = 1;
+    var fpsCost = 25;
 
     //タッチし始めの場所を確認
     var tapPos = new TapPos();
@@ -53,6 +53,9 @@ window.onload = function ()
     //UI・フォント
     core.preload('img/CP.png');
     core.preload('img/rednumber_siro.png');
+    core.preload('img/huki_blue.png');
+    core.preload('img/huki_green.png');
+    core.preload('img/huki_red.png');
 
     //fpsの設定
     core.fps = 30;
@@ -62,63 +65,89 @@ window.onload = function ()
         //フレームリセット
         core.frame = 0;
 
-        //スプライト情報の入力
-        //ボタン
+        ////////画像情報処理////////
+
+        //ププのボタン
         var pupuBtn = new Sprite(1200, 1200);
         pupuBtn.image = core.assets['img/pupu.png'];
-        pupuBtn.scale(0.05, 0.05);
-        pupuBtn.x = -50;
-        pupuBtn.y = -500;
-        pupuBtn.frame = 1;
+        pupuBtn.scale(0.25, 0.25);
+        pupuBtn.x = 2200;
+        pupuBtn.y = -300;
 
+        //ポポのボタン
         var popoBtn = new Sprite(1200, 1200);
         popoBtn.image = core.assets['img/popo.png'];
-        popoBtn.scale(0.05, 0.05);
-        popoBtn.x = -50;
-        popoBtn.y = -400;
-        popoBtn.frame = 1;
+        popoBtn.scale(0.25, 0.25);
+        popoBtn.x = 2200;
+        popoBtn.y = 300;
 
+        //ピピのボタン
         var pipiBtn = new Sprite(1200, 1200);
         pipiBtn.image = core.assets['img/pipi.png'];
-        pipiBtn.scale(0.05, 0.05);
-        pipiBtn.x = -50;
-        pipiBtn.y = -300;
-        pipiBtn.frame = 1;
+        pipiBtn.scale(0.25, 0.25);
+        pipiBtn.x = 2200;
+        pipiBtn.y = 900;
 
         //背景
-        var back = new Sprite(640, 360);
+        var back = new Sprite(3200, 1800);
         back.image = core.assets['img/back5.png'];
         back.x = 0;
         back.y = 0;
-        back.frame = 1;
 
-        //UI・フォント
+        //UI
+        //ププのUI背景
+        var PUPU_UI = new Sprite(600, 600);
+        PUPU_UI.image = core.assets['img/huki_red.png'];
+        PUPU_UI.scale(1.2, 1.2);
+        PUPU_UI.x = 1900;
+        PUPU_UI.y = 0;
+
+        //ポポのUI背景
+        var POPO_UI = new Sprite(600, 600);
+        POPO_UI.image = core.assets['img/huki_green.png'];
+        POPO_UI.scale(1.2, 1.2);
+        POPO_UI.x = 1900;
+        POPO_UI.y = 600;
+
+        //ピピのUI背景
+        var PIPI_UI = new Sprite(600, 600);
+        PIPI_UI.image = core.assets['img/huki_blue.png'];
+        PIPI_UI.scale(1.2, 1.2);
+        PIPI_UI.x = 1900;
+        PIPI_UI.y = 1200;
+
+        //矢印
+        var Arrow = new Sprite(600, 600);
+        Arrow.image = core.assets['img/ya_blue.png'];
+        Arrow.scale(0.5, 0.5);
+        Arrow.x = 5000;
+        Arrow.y = -5000;
+
+        //CPのフォント
         var CPFont = new Sprite(150, 150);
         CPFont.image = core.assets['img/CP.png'];
-        CPFont.scale(0.35, 0.35);
-        CPFont.x = 200;
-        CPFont.y = 250;
-        CPFont.frame = 1;
+        CPFont.scale(1, 1);
+        CPFont.x = 1300;
+        CPFont.y = 1600;
 
+        //コストのフォント
         var CostFont = new Array();
-
-        //桁数(初期設定4桁)
-        var costDigit = 4;
-
+        var costDigit = 4;  //桁数(初期設定4桁)
         for (var i = 0; i < costDigit; i++)
         {
             CostFont[i] = new Sprite(120, 120);
             CostFont[i].image = core.assets['img/rednumber_siro.png'];
-            CostFont[i].x = 200 - (i + 1) * 45;
-            CostFont[i].y = 250;
+            CostFont[i].scale(4, 4);
+            CostFont[i].x = 1300 - (i + 1) * 150;
+            CostFont[i].y = 1600;
             CostFont[i].frame = 0;
         }
 
         ////////メイン処理////////
-
+        //フレームごとに処理する
         core.addEventListener('enterframe', function ()
         {
-            if (core.frame % core.fps == 0);
+            if (core.frame % core.fps == 0)
             {
                 if (haveCost < MaxCost)
                     haveCost += fpsCost;
@@ -127,7 +156,8 @@ window.onload = function ()
             }
 
             //CPフォント
-            for (var i = costDigit; i >= 0; i--) {
+            for (var i = costDigit; i >= 0; i--)
+            {
                 FontSet(haveCost, i, CostFont[i]);
             }
         });
@@ -158,8 +188,8 @@ window.onload = function ()
             tapPos.y = startPos.y;
         });
 
-        //ボタンで離された時の処理
-        pupuBtn.on('touchend', function (startPos) {
+        //離された時の処理
+        pupuBtn.on('touchend', function () {
             pupuBtn.image = core.assets['img/pupu.png'];
         });
 
@@ -198,19 +228,50 @@ window.onload = function ()
                 if (Flag == "Succes")
                     PushDemon(PIPI, pipiBtn, tapPos, endPos, 1);
             }
+
+            tapObj = null;
         });
 
         ////////描画////////
         //オブジェクトに追加する処理(ここに入れたいオブジェクトを描画順に指定)
         core.rootScene.addChild(back);  //一番背景
-        core.rootScene.addChild(pupuBtn);  //一番前面
-        core.rootScene.addChild(popoBtn);  //一番前面
-        core.rootScene.addChild(pipiBtn);  //一番前面
-        core.rootScene.addChild(CPFont);  //一番前面
+
+        core.rootScene.addChild(pupuBtn);
+        core.rootScene.addChild(popoBtn);
+        core.rootScene.addChild(pipiBtn);
+
+        core.rootScene.addChild(PUPU_UI);
+        core.rootScene.addChild(POPO_UI);
+        core.rootScene.addChild(PIPI_UI);
+
+        core.rootScene.addChild(CPFont);
         for (var i = 0; i < costDigit; i++) {
             core.rootScene.addChild(CostFont[i]);  //一番前面
         }
-        
+
+        //矢印表示のためにここに処理
+        core.rootScene.on('touchmove', function (nowPos) {
+            //ププボタンの場所で押してた場合
+            if (tapObj == "pupuBtn") {
+                Arrow = ArrowSet(PUPU, pipiBtn, tapPos, nowPos, Arrow, core);
+                core.rootScene.addChild(Arrow);
+            }
+                //ポポボタンの場所で押してた場合
+            else if (tapObj == "popoBtn") {
+                Arrow = ArrowSet(POPO, pipiBtn, tapPos, nowPos, Arrow, core);
+                core.rootScene.addChild(Arrow);
+            }
+                //ピピボタンの場所で押してた場合
+            else if (tapObj == "pipiBtn") {
+                Arrow = ArrowSet(PIPI, pipiBtn, tapPos, nowPos, Arrow, core);
+                core.rootScene.addChild(Arrow);
+            }
+        });
+
+        core.rootScene.on('touchend', function () {
+            Arrow.x = 9000;
+            Arrow.y = -9000;
+        });
     }
     core.start();
 };
@@ -232,7 +293,6 @@ function TapPos(x, y) {
     this.y = y;
 }
 
-//CPフォントのセット
 function FontSet(_haveCost, Digit, Sprite) {
     if (Digit == 3) {
         Sprite.frame = _haveCost / 1000;
@@ -267,6 +327,104 @@ function CostCheck(_haveCost, demon) {
         _haveCost -= (demon.Cost + demon.Level * 10);
     }
     return _haveCost;
+}
+
+function ArrowSet(demon, btn, startPos, endPos, Arrow, core)
+{
+    //座標の移動幅を見て方向指定
+    //上方向時
+    if ((startPos.y - endPos.y) > btn.height / 2 * btn.scaleY)
+    {
+        if (demon.Type == "PUPU")
+        {
+            Arrow.image = core.assets['img/ya_red.png'];
+            Arrow.x = 2500;
+            Arrow.y = -200;
+            Arrow.rotation = 0;
+        }
+        else if (demon.Type == "POPO")
+        {
+            Arrow.image = core.assets['img/ya_green.png'];
+            Arrow.x = 2500;
+            Arrow.y = 400;
+            Arrow.rotation = 0;
+        }
+        else if (demon.Type == "PIPI")
+        {
+            Arrow.image = core.assets['img/ya_blue.png'];
+            Arrow.x = 2500;
+            Arrow.y = 1000;
+            Arrow.rotation = 0;
+        }        
+    }
+    //下方向時
+    else if ((startPos.y - endPos.y) < -btn.height / 2 * btn.scaleY)
+    {
+        if (demon.Type == "PUPU") {
+            Arrow.image = core.assets['img/ya_red.png'];
+            Arrow.x = 2500;
+            Arrow.y = 200;
+            Arrow.rotation = 180;
+        }
+        else if (demon.Type == "POPO") {
+            Arrow.image = core.assets['img/ya_green.png'];
+            Arrow.x = 2500;
+            Arrow.y = 800;
+            Arrow.rotation = 180;
+        }
+        else if (demon.Type == "PIPI") {
+            Arrow.image = core.assets['img/ya_blue.png'];
+            Arrow.x = 2500;
+            Arrow.y = 1400;
+            Arrow.rotation = 180;
+        }
+    }
+    //右方向時
+    else if ((startPos.x - endPos.x) < -btn.height / 2 * btn.scaleX)
+    {
+        if (demon.Type == "PUPU") {
+            Arrow.image = core.assets['img/ya_red.png'];
+            Arrow.x = 2700;
+            Arrow.y = 0;
+            Arrow.rotation = 90;
+        }
+        else if (demon.Type == "POPO") {
+            Arrow.image = core.assets['img/ya_green.png'];
+            Arrow.x = 2700;
+            Arrow.y = 600;
+            Arrow.rotation = 90;
+        }
+        else if (demon.Type == "PIPI") {
+            Arrow.image = core.assets['img/ya_blue.png'];
+            Arrow.x = 2700;
+            Arrow.y = 1200;
+            Arrow.rotation = 90;
+        }
+    }
+    //左方向時
+    else if ((startPos.x - endPos.x) > btn.height / 2 * btn.scaleX)
+    {
+        if (demon.Type == "PUPU") {
+            Arrow.image = core.assets['img/ya_red.png'];
+            Arrow.x = 2300;
+            Arrow.y = 0;
+            Arrow.rotation = 270;
+        }
+        else if (demon.Type == "POPO") {
+            Arrow.image = core.assets['img/ya_green.png'];
+            Arrow.x = 2300;
+            Arrow.y = 600;
+            Arrow.rotation = 270;
+        }
+        else if (demon.Type == "PIPI") {
+            Arrow.image = core.assets['img/ya_blue.png'];
+            Arrow.x = 2300;
+            Arrow.y = 1200;
+            Arrow.rotation = 270;
+        }
+    }
+
+    return Arrow;
 }
 
 //デーモンの送信
