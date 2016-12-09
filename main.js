195161bd1b009eb07a46b397e0bbfe68e7d53615
Hotta_Yutaro <g1327061@st.t-kougei.ac.jp>
2016-12-09 18:49:42 +0900

    no message

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var socket = io.connect('https://safe-reef-35714.herokuapp.com/');
-//var socket = io.connect('ws://192.168.11.250:5555/');
+//var socket = io.connect('https://safe-reef-35714.herokuapp.com/');
+var socket = io.connect('ws://192.168.198.144:5555/');
 //var socket = io.connect('ws://localhost:5555/');
 //var socket = io.connect('ws://192.168.11.250:5555/', {transports: ["websocket"]});
 
